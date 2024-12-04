802f0f7c48fe511233d17eda80760241ed6bbcb6
Konstantin Diachenko <drakonard@gmail.com>
2024-12-05 00:30:30 +0530

    Make tests shorter

--- Foundation.Data.Doublets.Cli.Tests/UnitTest1.cs
+++ Foundation.Data.Doublets.Cli.Tests/UnitTest1.cs
@@ -7,241 +7,173 @@ using static Foundation.Data.Doublets.Cli.QueryProcessor;
 
 namespace Foundation.Data.Doublets.Cli.Tests.Tests
 {
-  public class LinkCliTests
-  {
-    [Fact]
-    public void CreateSingleLinkTest()
+    public class LinkCliTests
     {
-      // Arrange
-      string tempDbFile = Path.GetTempFileName();
-      try
-      {
-        using var links = new UnitedMemoryLinks<uint>(tempDbFile);
-        var query = "(() ((1 1)))";
+        [Fact]
+        public void CreateSingleLinkTest()
+        {
+            RunTestWithLinks(links =>
+            {
+                // Act
+                ProcessQuery(links, "(() ((1 1)))");
 
-        // Act
-        ProcessQuery(links, query);
+                // Assert
+                var allLinks = GetAllLinks(links);
+                Assert.Single(allLinks);
+                AssertLinkExists(allLinks, 1, 1, 1);
+            });
+        }
 
-        // Assert
-        var allLinks = GetAllLinks(links);
-        Assert.Single(allLinks);
-        AssertLinkExists(allLinks, index: 1, source: 1, target: 1);
-      }
-      finally
-      {
-        File.Delete(tempDbFile);
-      }
+        [Fact]
+        public void CreateLinkWithSource2Target2Test()
+        {
+            RunTestWithLinks(links =>
+            {
+                // Act
+                ProcessQuery(links, "(() ((1 1)))");
+                ProcessQuery(links, "(() ((2 2)))");
+
+                // Assert
+                var allLinks = GetAllLinks(links);
+                Assert.Equal(2, allLinks.Count);
+                AssertLinkExists(allLinks, 1, 1, 1);
+                AssertLinkExists(allLinks, 2, 2, 2);
+            });
+        }
+
+        [Fact]
+        public void CreateMultipleLinksTest()
+        {
+            RunTestWithLinks(links =>
+            {
+                // Act
+                ProcessQuery(links, "(() ((1 1) (2 2)))");
+
+                // Assert
+                var allLinks = GetAllLinks(links);
+                Assert.Equal(2, allLinks.Count);
+                AssertLinkExists(allLinks, 1, 1, 1);
+                AssertLinkExists(allLinks, 2, 2, 2);
+            });
+        }
+
+        [Fact]
+        public void UpdateSingleLinkTest()
+        {
+            RunTestWithLinks(links =>
+            {
+                // Arrange
+                ProcessQuery(links, "(() ((1 1)))");
+                ProcessQuery(links, "(() ((2 2)))");
+
+                // Act
+                ProcessQuery(links, "(((1: 1 1)) ((1: 1 2)))");
+
+                // Assert
+                var allLinks = GetAllLinks(links);
+                Assert.Equal(2, allLinks.Count);
+                AssertLinkExists(allLinks, 1, 1, 2);
+                AssertLinkExists(allLinks, 2, 2, 2);
+            });
+        }
+
+        [Fact]
+        public void MultipleUpdatesTest()
+        {
+            RunTestWithLinks(links =>
+            {
+                // Arrange
+                ProcessQuery(links, "(() ((1 1) (2 2)))");
+
+                // Act
+                ProcessQuery(links, "(((1: 1 1) (2: 2 2)) ((1: 1 2) (2: 2 1)))");
+
+                // Assert
+                var allLinks = GetAllLinks(links);
+                Assert.Equal(2, allLinks.Count);
+                AssertLinkExists(allLinks, 1, 1, 2);
+                AssertLinkExists(allLinks, 2, 2, 1);
+            });
+        }
+
+        [Fact]
+        public void DeleteSingleLinkTest_Source1Target2()
+        {
+            RunTestWithLinks(links =>
+            {
+                // Arrange
+                ProcessQuery(links, "(() ((1 2)))");
+                ProcessQuery(links, "(() ((2 2)))");
+
+                // Act
+                ProcessQuery(links, "(((1 2)) ())");
+
+                // Assert
+                var allLinks = GetAllLinks(links);
+                Assert.Single(allLinks);
+                AssertLinkExists(allLinks, 2, 2, 2);
+            });
+        }
+
+        [Fact]
+        public void DeleteSingleLinkTest_Source2Target2()
+        {
+            RunTestWithLinks(links =>
+            {
+                // Arrange
+                ProcessQuery(links, "(() ((2 2)))");
+
+                // Act
+                ProcessQuery(links, "(((2 2)) ())");
+
+                // Assert
+                var allLinks = GetAllLinks(links);
+                Assert.Empty(allLinks);
+            });
+        }
+
+        [Fact]
+        public void DeleteMultipleLinksTest()
+        {
+            RunTestWithLinks(links =>
+            {
+                // Arrange
+                ProcessQuery(links, "(() ((1 2) (2 2)))");
+
+                // Act
+                ProcessQuery(links, "(((1 2) (2 2)) ())");
+
+                // Assert
+                var allLinks = GetAllLinks(links);
+                Assert.Empty(allLinks);
+            });
+        }
+
+        // Helper methods
+        private static void RunTestWithLinks(Action<ILinks<uint>> testAction)
+        {
+            string tempDbFile = Path.GetTempFileName();
+            try
+            {
+                using var links = new UnitedMemoryLinks<uint>(tempDbFile);
+                testAction(links);
+            }
+            finally
+            {
+                File.Delete(tempDbFile);
+            }
+        }
+
+        private static List<DoubletLink> GetAllLinks(ILinks<uint> links)
+        {
+            var any = links.Constants.Any;
+            var query = new DoubletLink(index: any, source: any, target: any);
+            return links.All(query).Select(doublet => new DoubletLink(doublet)).ToList();
+        }
+
+        private static void AssertLinkExists(List<DoubletLink> allLinks, uint index, uint source, uint target)
+        {
+            Assert.Contains(allLinks, link => link.Index == index && link.Source == source && link.Target == target);
+        }
     }
-
-    [Fact]
-    public void CreateLinkWithSource2Target2Test()
-    {
-      // Arrange
-      string tempDbFile = Path.GetTempFileName();
-      try
-      {
-        using var links = new UnitedMemoryLinks<uint>(tempDbFile);
-
-        // First, create initial link
-        ProcessQuery(links, "(() ((1 1)))");
-
-        var query = "(() ((2 2)))";
-
-        // Act
-        ProcessQuery(links, query);
-
-        // Assert
-        var allLinks = GetAllLinks(links);
-        Assert.Equal(2, allLinks.Count);
-        AssertLinkExists(allLinks, index: 1, source: 1, target: 1);
-        AssertLinkExists(allLinks, index: 2, source: 2, target: 2);
-      }
-      finally
-      {
-        File.Delete(tempDbFile);
-      }
-    }
-
-    [Fact]
-    public void CreateMultipleLinksTest()
-    {
-      // Arrange
-      string tempDbFile = Path.GetTempFileName();
-      try
-      {
-        using var links = new UnitedMemoryLinks<uint>(tempDbFile);
-        var query = "(() ((1 1) (2 2)))";
-
-        // Act
-        ProcessQuery(links, query);
-
-        // Assert
-        var allLinks = GetAllLinks(links);
-        Assert.Equal(2, allLinks.Count);
-        AssertLinkExists(allLinks, index: 1, source: 1, target: 1);
-        AssertLinkExists(allLinks, index: 2, source: 2, target: 2);
-      }
-      finally
-      {
-        File.Delete(tempDbFile);
-      }
-    }
-
-    [Fact]
-    public void UpdateSingleLinkTest()
-    {
-      // Arrange
-      string tempDbFile = Path.GetTempFileName();
-      try
-      {
-        using var links = new UnitedMemoryLinks<uint>(tempDbFile);
-
-        // Create initial links
-        ProcessQuery(links, "(() ((1 1)))");
-        ProcessQuery(links, "(() ((2 2)))");
-
-        var query = "(((1: 1 1)) ((1: 1 2)))";
-
-        // Act
-        ProcessQuery(links, query);
-
-        // Assert
-        var allLinks = GetAllLinks(links);
-        Assert.Equal(2, allLinks.Count);
-        AssertLinkExists(allLinks, index: 1, source: 1, target: 2);
-        AssertLinkExists(allLinks, index: 2, source: 2, target: 2);
-      }
-      finally
-      {
-        File.Delete(tempDbFile);
-      }
-    }
-
-
-    [Fact]
-    public void MultipleUpdatesTest()
-    {
-      // Arrange
-      string tempDbFile = Path.GetTempFileName();
-      try
-      {
-        using var links = new UnitedMemoryLinks<uint>(tempDbFile);
-
-        // Create initial links
-        ProcessQuery(links, "(() ((1 1) (2 2)))");
-
-        var query = "(((1: 1 1) (2: 2 2)) ((1: 1 2) (2: 2 1)))";
-
-        // Act
-        ProcessQuery(links, query);
-
-        // Assert
-        var allLinks = GetAllLinks(links);
-        Assert.Equal(2, allLinks.Count);
-        AssertLinkExists(allLinks, index: 1, source: 1, target: 2);
-        AssertLinkExists(allLinks, index: 2, source: 2, target: 1);
-      }
-      finally
-      {
-        File.Delete(tempDbFile);
-      }
-    }
-    
-    [Fact]
-    public void DeleteSingleLinkTest_Source1Target2()
-    {
-      // Arrange
-      string tempDbFile = Path.GetTempFileName();
-      try
-      {
-        using var links = new UnitedMemoryLinks<uint>(tempDbFile);
-
-        // Create initial links
-        ProcessQuery(links, "(() ((1 2)))");
-        ProcessQuery(links, "(() ((2 2)))");
-
-        var query = "(((1 2)) ())";
-
-        // Act
-        ProcessQuery(links, query);
-
-        // Assert
-        var allLinks = GetAllLinks(links);
-        Assert.Single(allLinks);
-        AssertLinkExists(allLinks, index: 2, source: 2, target: 2);
-      }
-      finally
-      {
-        File.Delete(tempDbFile);
-      }
-    }
-
-    [Fact]
-    public void DeleteSingleLinkTest_Source2Target2()
-    {
-      // Arrange
-      string tempDbFile = Path.GetTempFileName();
-      try
-      {
-        using var links = new UnitedMemoryLinks<uint>(tempDbFile);
-
-        // Create initial link
-        ProcessQuery(links, "(() ((2 2)))");
-
-        var query = "(((2 2)) ())";
-
-        // Act
-        ProcessQuery(links, query);
-
-        // Assert
-        var allLinks = GetAllLinks(links);
-        Assert.Empty(allLinks);
-      }
-      finally
-      {
-        File.Delete(tempDbFile);
-      }
-    }
-
-    [Fact]
-    public void DeleteMultipleLinksTest()
-    {
-      // Arrange
-      string tempDbFile = Path.GetTempFileName();
-      try
-      {
-        using var links = new UnitedMemoryLinks<uint>(tempDbFile);
-
-        // Create initial links
-        ProcessQuery(links, "(() ((1 2) (2 2)))");
-
-        var query = "(((1 2) (2 2)) ())";
-
-        // Act
-        ProcessQuery(links, query);
-
-        // Assert
-        var allLinks = GetAllLinks(links);
-        Assert.Empty(allLinks);
-      }
-      finally
-      {
-        File.Delete(tempDbFile);
-      }
-    }
-
-    // Helper methods
-    private static List<DoubletLink> GetAllLinks(ILinks<uint> links)
-    {
-      var allLinks = new List<DoubletLink>();
-      var any = links.Constants.Any;
-      var query = new DoubletLink(index: any, source: any, target: any);
-      return links.All(query).Select(doublet => new DoubletLink(doublet)).ToList();
-    }
-
-    private static void AssertLinkExists(List<DoubletLink> allLinks, uint index, uint source, uint target)
-    {
-      Assert.Contains(allLinks, link => link.Index == index && link.Source == source && link.Target == target);
-    }
-  }
 }
